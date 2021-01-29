4d90118013da6580684198dbfe7dc10b42903f8c
Faraz Khonsari <farazbern2@gmail.com>
2021-01-29 23:53:03 +0330

    Update PlantDetailFragmentTest.kt

--- app/src/androidTest/java/com/google/samples/apps/sunflower/PlantDetailFragmentTest.kt
+++ app/src/androidTest/java/com/google/samples/apps/sunflower/PlantDetailFragmentTest.kt
@@ -39,48 +39,8 @@ import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-@RunWith(AndroidJUnit4::class)
+ 
 class PlantDetailFragmentTest {
 
-    @Rule
-    @JvmField
-    val activityTestRule = ActivityTestRule(GardenActivity::class.java)
-
-    @Before
-    fun jumpToPlantDetailFragment() {
-        activityTestRule.activity.apply {
-            runOnUiThread {
-                val bundle = Bundle().apply { putString("plantId", testPlant.plantId) }
-                findNavController(R.id.nav_host).navigate(R.id.plant_detail_fragment, bundle)
-            }
-        }
-    }
-
-    @Ignore("Share button redesign pending")
-    @Test
-    fun testShareTextIntent() {
-        val shareText = activityTestRule.activity.getString(
-            R.string.share_text_plant,
-            testPlant.name
-        )
-
-        Intents.init()
-        onView(withId(R.id.action_share)).perform(click())
-        intended(
-            chooser(
-                allOf(
-                    hasAction(Intent.ACTION_SEND),
-                    hasType("text/plain"),
-                    hasExtra(Intent.EXTRA_TEXT, shareText)
-                )
-            )
-        )
-        Intents.release()
-
-        // dismiss the Share Dialog
-        InstrumentationRegistry.getInstrumentation()
-            .uiAutomation
-            .performGlobalAction(AccessibilityService.GLOBAL_ACTION_BACK)
-    }
+    
 }
